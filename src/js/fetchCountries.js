656364fb5413bc6ffa15cd571620f1fd24f2db2b
Yana Mironyak <yanika.mironyak@gmail.com>
2022-05-21 17:50:12 +0300

    Update

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,5 @@
 export default function fetchCountries(name) {
 
-    return fetch('https://restcountries.com/v2/{name}?fields=name.official,capital,population,flag.svg,languages').then(response => {console.log(response.json()) });
+    return fetch(`'https://restcountries.com/v3.1/${name}?fields=name.official,capital,population,flag.svg,languages'`)
+        .then(response => response.json()).then(data => { console.log(data) });
 }
